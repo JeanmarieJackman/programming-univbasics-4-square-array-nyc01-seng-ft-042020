752ec7cf28cdb7fa6fc488094023cf9b5bc63c92
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-11 16:20:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,12 @@
 # your code here
 array = []
+
 def square_array(array)
 sqaured_array = [] 
 counter = 0 
-while counter < array.length 
 array.each do |number| 
   sqaured_array << (number * number)
   counter += 1 
   end
-end 
 sqaured_array
-
-
+end
