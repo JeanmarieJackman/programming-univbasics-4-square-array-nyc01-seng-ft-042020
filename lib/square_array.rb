319659bed31c60c123949ba57a8128332ff176b5
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-11 16:10:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+# your code here
+counter = 0 
+numbers = []
+def square_array(numbers)
+  while counter < numbers.length do 
+  puts square_array.push(numbers ** 2)
+  counter += 1 
+  end 
 end
+square_array
