e316ae0f4b41a5d9e6bf4f1968f732de05ed4661
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-11 16:23:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ def square_array(array)
 sqaured_array = [] 
 counter = 0 
 array.length.times do |index| 
-  sqaured_array.push(array[index]*[index])
+  sqaured_array.push(array[index]**2)
   counter += 1 
   end
 sqaured_array
